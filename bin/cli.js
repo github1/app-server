97a22ceafd902e2ce5e38b961dfe32ddac82dd6c
github1 <john.collier4@gmail.com>
2019-04-27 22:25:01 -0400

    prepare app-server package for publish

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
-const fs = require('fs');
 const path = require('path');
 const server = require('../src/index.js');
 const baseDir = process.env.SERVICE_BASE_DIR || process.cwd();
 const serverJS = path.join(baseDir, 'server.js');
 
-server([serverJS, {
-        onStart: context => {
-            console.log('Listening on port ' + context.port);
-        }
-    }
-]);
+server([serverJS]);
