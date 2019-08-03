6178e43bfd7c5f7c8e951bf7ac7e8bcf64d803da
github1 <john.collier4@gmail.com>
2019-08-03 10:41:18 -0400

    add ability to provide external package preparation before publish
    ref-3fa183849cc0be2ce863d82de9fe3a13e95c59f1

--- prepare.sh
+++ prepare.sh
@@ -4,3 +4,6 @@ DIST="./dist"
 rm -rf "${DIST}"
 mkdir -p "${DIST}"
 cp -R assets bin src templates package.json "${DIST}/"
+if [[ -f "${AUX_PREPARE_SCRIPT}" ]]; then
+  "${AUX_PREPARE_SCRIPT}" "${DIST}/"
+fi
