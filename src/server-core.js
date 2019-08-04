214208c9a0b91f071abaa5f271f4436fc0ab764f
github1 <john.collier4@gmail.com>
2019-08-04 00:38:25 -0400

    require smerge inside server init function
    ref-8f6283923886310c1fcceccf02f51d5032215b10

--- src/server-core.js
+++ src/server-core.js
@@ -6,12 +6,13 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const serveStatic = require('./serve-static-fallback');
 const amfMiddleware = require('./amf-middleware');
-const smerge = require('@github1/smerge');
 const oneDay = 86400000;
 const cacheRegExp = /\/c[0-9^\/]+\//;
 const cacheKey = 'c' + new Date().getTime();
 
 module.exports = (process, express, port, extensions) => {
+
+    const smerge = require('@github1/smerge');
 
     extensions = extensions || [];
 
