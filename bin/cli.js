a7c4ff0a86aeb0eaa5989056550df80dadd15a45
github1 <john.collier4@gmail.com>
2019-12-06 23:41:40 -0500

    ability to load all server extensions present in service baseDir

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 const path = require('path');
 const server = require('../src/index.js');
+const fs = require('fs');
+
 let serverJS = process.argv[2];
 if (serverJS) {
-  serverJS = path.resolve(serverJS);
+  serverJS = [path.resolve(serverJS)];
 } else {
-  serverJS = path.resolve((process.env.SERVICE_BASE_DIR || process.cwd()), 'server.js');
+  const basedir = (process.env.SERVICE_BASE_DIR || process.cwd());
+  serverJS = fs.readdirSync(basedir)
+    .filter((file) => process.env.LOAD_ALL_SERVICES ? /^server\..*js$/.test(file) : 'server.js' === file)
+    .map((file) => path.resolve(basedir, file));
 }
-server([serverJS]);
+server(serverJS);
